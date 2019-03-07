ca5acc1cbd9af3123f1dcc7099b96d7588869f6c
hakan.erdem <hakan.erdem@r210-211.schule.local>
2019-03-07 10:31:40 +0100

    harshad gemacht anagram angefangen

--- Einfuehrung/src/passwortchecker.java
+++ Einfuehrung/src/passwortchecker.java
@@ -1,73 +1,105 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class passwortchecker {
-
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		zuCharArray("fsadoi.,");
-	}
-public static void zuCharArray(String test)
+public class passwortchecker 
 {
-	char[] charArray = test.toCharArray();
-	
-	
-	for(int i = 0;i < charArray.length;i++)
-	
+	static char[] charArray;
+
+	public static void main(String[] args)
 	{
-	//System.out.println(charArray[i]);
-
-if(charArray.length >= 8)
-{
-	System.out.println("true");
-
-
-if (Character.isUpperCase(charArray[i]))
-		{
-			System.out.println("true");
-			
-
 		
-	if(Character.isDigit(charArray[i]))
-			{ 
-		System.out.println("true");
-
-			} 
-			
-			if(!Character.isLetterOrDigit(charArray[i]))
-			{
-				System.out.println("true");
-
-			
-			}
-			if(Character.isLowerCase(charArray[i]))
-
-			{
-				
-				System.out.println("true");
+		//checker("1bestePasswort2");
+		//pwCheck("hallooooooooO");
+		
+		harshad(100);
+	}
 	
+	/*public static void checker(String passwort)
+	{
+		System.out.println(passwort);
+		//System.out.println(charArray[3]);
+		System.out.println();
+		charArray = passwort.toCharArray();
+		for (int i = 0; i < charArray.length; i++)
+		{
+			if (Character.isUpperCase(charArray[i]))
+			{
+				System.out.println(charArray[i]+ " ist GROß");
 			}
-		
-
-				
-			
-			
-		}	
-
+			else if (Character.isDigit(charArray[i]))
+			{
+				System.out.println(charArray[i] + " ist Zahl");
+			}
+			else
+			{
+				System.out.println(charArray[i] + " ist klein");
+			}
+		}
+	}*/
+	
+	public static boolean pwCheck(String passwort)
+	{
+		System.out.println(passwort);
+		charArray = passwort.toCharArray();
+		if(passwort=="Perzer0bfg00a3")
+		{
+			System.out.println("Ernsthaft?");
+			return false;
+		}
+		if (charArray.length >= 8)
+		{
+			for (int i=0; i < charArray.length; i++)
+			{
+				if(Character.isUpperCase(charArray[i]))
+				{
+					for (int j=0; j < charArray.length; j++)
+					{
+						if(Character.isLowerCase(charArray[j]))
+						{
+							for (int k=0; k < charArray.length; k++)
+							{
+								if(Character.isDigit(charArray[k]))
+								{
+									for (int l=0; l < charArray.length; l++)
+									{
+										if(!Character.isLetterOrDigit(charArray[l]))
+										{
+											System.out.print("Alles okay"); 
+											return true;
+										}
+									} System.out.print("1 Sonderzeichen fehlt!"); return false;
+								}
+							}System.out.print("1 Zahl fehlt"); return false;
+						}
+					}System.out.print("1 klein Buchstabe fehlt"); return false;
+				}
+			}System.out.print("1 groß Buchstabe fehlt"); return false;
+		}else
+		{
+			System.out.println("8 Zeichen sind nötig!");
+			return false;
+		}
+	}
+	
+	
+	public static int quersumme(int zahl)
+	{
+		if(zahl<=9) return zahl;
+		return zahl%10 + quersumme(zahl/10);
 	}
 
-}
-}
-}
-		
+
+public static void harshad(int ende)
+{
+	for( int i=1; i<=ende; i++)
+	{
+		if(i%quersumme(i)==0)
+		{
+	System.out.println(i);
 	
+		}
+		}
+	
+}
 
-
-
-
-
-
+}	
 
 
 
