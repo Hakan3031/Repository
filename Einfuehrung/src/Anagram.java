370d1445264e478b1c7ed2085fca56ebd5b6a641
hakan.erdem <hakan.erdem@r210-211.schule.local>
2019-03-07 10:40:33 +0100

    neues

---
+++ Einfuehrung/src/Anagram.java
@@ -0,0 +1,28 @@
+
+public class Anagram {
+	static char[] charArray;
+static int wort;
+static int wort2;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+public static void anagram(String wort)
+
+{
+	System.out.println(wort);
+	charArray = wort.toCharArray();
+}
+public static void anagramx(String wort2)
+{
+	System.out.println(wort2);
+	charArray = wort2.toCharArray();
+    wort2.toLowerCase();
+    
+}
+
+
+}
+
